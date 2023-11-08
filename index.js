96c185788204a6a34a15c91ee04dd49fe1e96e32
Kayla Lebron <kayla.lebron12@gmail.com>
2023-11-08 00:08:13 -0500

    hey wassup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    const uppercaseQuery = query.toUpperCase();
+    const matchingDrivers = drivers.filter(driver => {
+      const uppercaseDriverName = driver.toUpperCase();
+      return uppercaseDriverName.includes(uppercaseQuery);
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch (drivers, query){
+    return drivers.filter((driver) => driver.startsWith(query))
+
+}
+ function matchName (driver, query){
+    return driver.filter((driver => driver.name === query))
+
+ }
